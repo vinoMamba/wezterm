25bd9f9e7c7c534c15d04196577e87a106dcf846
vino <vino0908@outlook.com>
2024-02-24 17:20:53 +0800

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,24 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-
 local config = {}
 -- Use config builder object if possible
 if wezterm.config_builder then config = wezterm.config_builder() end
+
+config.background = {
+    {
+      source = { File = "C:\\Users\\wx\\.config\\wezterm\\images\\astro.jpg" },
+      opacity = 0.9
+    },
+  }
+
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 5
+}
+
 
 -- Settings
 config.default_domain= "WSL:Arch"
@@ -17,7 +31,6 @@ config.window_background_opacity = 0.8
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.scrollback_lines = 3000
-config.default_workspace = "main"
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
@@ -43,6 +56,7 @@ config.keys = {
   { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = false } },
   { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
   { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
+  { key = "f",          mods = "ALT",         action = act.ToggleFullScreen },
 
   -- We can make separate keybindings for resizing panes
   -- But Wezterm offers custom "mode" in the name of "KeyTable"
@@ -112,54 +126,18 @@ config.key_tables = {
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = false
-wezterm.on("update-status", function(window, pane)
-  -- Workspace name
-  local stat = window:active_workspace()
-  local stat_color = "#f7768e"
-  -- It's a little silly to have workspace name all the time
-  -- Utilize this to display LDR or current key table name
-  if window:active_key_table() then
-    stat = window:active_key_table()
-    stat_color = "#7dcfff"
-  end
-  if window:leader_is_active() then
-    stat = "LDR"
-    stat_color = "#bb9af7"
-  end
-
-  -- Current working directory
-  local basename = function(s)
-    -- Nothing a little regex can't fix
-    return string.gsub(s, "(.*[/\\])(.*)", "%2")
-  end
-  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-  local cwd = pane:get_current_working_dir()
-  cwd = cwd and basename(cwd) or ""
-  -- Current command
-  local cmd = pane:get_foreground_process_name()
-  cmd = cmd and basename(cmd) or ""
-
+wezterm.on("update-status", function(window)
   -- Time
   local time = wezterm.strftime("%H:%M")
 
   -- Left status (left of the tab line)
   window:set_left_status(wezterm.format({
-    { Foreground = { Color = stat_color } },
-    { Text = "  " },
-    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-    { Text = " |" },
+    { Text = " Vino" },
+    { Text = " | " },
   }))
 
   -- Right status
   window:set_right_status(wezterm.format({
-    -- Wezterm has a built-in nerd fonts
-    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-    { Text = " | " },
-    { Foreground = { Color = "#e0af68" } },
-    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-    "ResetAttributes",
-    { Text = " | " },
     { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
     { Text = "  " },
   }))
