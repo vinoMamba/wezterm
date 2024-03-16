2c877ec92a7d4aed9b82d8649aa391f26d950af8
王鑫 <wangxin@192.168.10.80>
2024-03-16 19:01:41 +0800

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,146 +1,126 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-local config = {}
--- Use config builder object if possible
-if wezterm.config_builder then config = wezterm.config_builder() end
+-- config
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 
--- config.background = {
---     {
---       source = { File = "C:\\Users\\vino0\\.config\\wezterm\\images\\astro.jpg" },
---       opacity = 0.9
---     },
---   }
+--Colors & Appearance
+config.color_scheme = 'Arthur'
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
   left = 10,
-  right = 10,
-  top = 10,
-  bottom = 5
+  right = 0,
+  top = 20,
+  bottom = 0,
 }
 
-
--- Settings
-config.default_domain= "WSL:Arch"
-
-config.color_scheme = "Arthur"
-config.font = wezterm.font_with_fallback({
-  { family = "JetBrains Mono",  scale = 1.24, weight = "Medium", },
-})
-config.window_background_opacity = 0.8
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "NeverPrompt"
-config.scrollback_lines = 3000
-
--- Dim inactive panes
 config.inactive_pane_hsb = {
-  saturation = 0.24,
-  brightness = 0.5
+  saturation = 0.9,
+  brightness = 0.8,
 }
 
--- Keys
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1001 }
-config.keys = {
-  -- Send C-a when pressing C-a twice
-  { key = "a",          mods = "LEADER|CTRL", action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
-  { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.3
 
-  -- Pane keybindings
-  { key = "j",          mods = "ALT",         action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-  { key = "l",          mods = "ALT",         action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "h",          mods = "CTRL",        action = act.ActivatePaneDirection("Left") },
-  { key = "j",          mods = "CTRL",        action = act.ActivatePaneDirection("Down") },
-  { key = "k",          mods = "CTRL",        action = act.ActivatePaneDirection("Up") },
-  { key = "l",          mods = "CTRL",        action = act.ActivatePaneDirection("Right") },
-  { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = false } },
-  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
-  { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
-  { key = "f",          mods = "ALT",         action = act.ToggleFullScreen },
-
-  -- We can make separate keybindings for resizing panes
-  -- But Wezterm offers custom "mode" in the name of "KeyTable"
-  { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
-
-  -- Tab keybindings
-  { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "-",          mods = "CTRL",            action = act.ActivateTabRelative(-1) },
-  { key = "=",          mods = "CTRL",            action = act.ActivateTabRelative(1) },
-  { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
+-- Fonts
+config.font = wezterm.font_with_fallback({
   {
-    key = "e",
-    mods = "LEADER",
-    action = act.PromptInputLine {
-      description = wezterm.format {
-        { Attribute = { Intensity = "Bold" } },
-        { Foreground = { AnsiColor = "Fuchsia" } },
-        { Text = "Renaming Tab Title...:" },
-      },
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:active_tab():set_title(line)
-        end
-      end)
-    }
+    family = "JetBrains Mono",
+    scale = 1.24,
+    weight = "Medium",
+    -- italic = true,
+    -- => != !==
+    -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   },
-  -- Key table for moving tabs around
-  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
-  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+})
 
-  -- Lastly, workspace
-  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+-- Key Bingding
 
+local act = wezterm.action
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    { key = 'r', mods = 'SUPER', action = act.ReloadConfiguration },
+    { key = 'q', mods = 'CTRL', action = act.QuitApplication },
+    -- tab
+    { key = 'n', mods = 'ALT',    action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 'q', mods = 'LEADER', action = act.CloseCurrentTab { confirm = false } },
+    { key = '=', mods = 'ALT', action = act.ActivateTabRelative(1) },
+    { key = '-', mods = 'ALT', action = act.ActivateTabRelative(-1) },
+    { key = 'f', mods = 'ALT', action = act.ToggleFullScreen },
+    -- move tab
+    { key = ']', mods = 'ALT', action = act.MoveTabRelative(1) },
+    { key = '[', mods = 'ALT', action = act.MoveTabRelative(-1) },
+
+    -- window
+    { key = 'N', mods = 'ALT', action = act.SpawnWindow },
+
+    -- font size
+    { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
+    { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+    { key = '0', mods = 'CTRL', action = act.ResetFontSize },
+
+    --panel
+    { key = 'j', mods = 'ALT', action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+    { key = 'l', mods = 'ALT', action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+    { key = 'q', mods = 'LEADER', action = act.CloseCurrentPane{ confirm = false } },
+
+    { key = 'h', mods = 'CTRL', action = act.ActivatePaneDirection 'Left' },
+    { key = 'j', mods = 'CTRL', action = act.ActivatePaneDirection 'Down' },
+    { key = 'k', mods = 'CTRL', action = act.ActivatePaneDirection 'Up' },
+    { key = 'l', mods = 'CTRL', action = act.ActivatePaneDirection 'Right' },
+    { key = 'z', mods = 'CTRL', action = act.TogglePaneZoomState },
+
+    { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize{ 'Left',  5 } },
+    { key = 'j', mods = 'LEADER', action = act.AdjustPaneSize{ 'Down',  5 } },
+    { key = 'k', mods = 'LEADER', action = act.AdjustPaneSize{ 'Up',    5 } },
+    { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize{ 'Right', 5 } },
+
+
+    { key = '\\', mods = 'ALT', action = act.QuickSelect },
+
+    { key = 'f', mods = 'CTRL', action = act.Search 'CurrentSelectionOrEmptyString' },
+    { key = 'm', mods = 'CTRL', action = act.Hide },
+    { key = 'P', mods = 'CTRL', action = act.ActivateCommandPalette },
 }
--- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
-for i = 1, 9 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = act.ActivateTab(i - 1)
-  })
-end
 
-config.key_tables = {
-  resize_pane = {
-    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
-    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
-    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
-    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
+-- Mouse Bingding
+config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
   },
-  move_tab = {
-    { key = "h",      action = act.MoveTabRelative(-1) },
-    { key = "j",      action = act.MoveTabRelative(-1) },
-    { key = "k",      action = act.MoveTabRelative(1) },
-    { key = "l",      action = act.MoveTabRelative(1) },
-    { key = "Escape", action = "PopKeyTable" },
-    { key = "Enter",  action = "PopKeyTable" },
-  }
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
 }
 
--- Tab bar
--- I don't like the look of "fancy" tab bar
-config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
-config.tab_bar_at_bottom = false
-wezterm.on("update-status", function(window)
-  -- Time
-  local time = wezterm.strftime("%H:%M")
+-- event: gui-startup
+local mux = wezterm.mux
 
-  -- Left status (left of the tab line)
-  window:set_left_status(wezterm.format({
-    { Text = " Vino" },
-    { Text = " | " },
-  }))
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():set_inner_size(1600, 1000)
+  window:gui_window():set_position(1000, 500)
 
-  -- Right status
-  window:set_right_status(wezterm.format({
-    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-    { Text = "  " },
-  }))
 end)
+
+
+-- config.status_update_interval = 1000
+-- wezterm.on("update-status", function(window)
+--
+--   window:set_right_status(wezterm.format({
+--     { Text = "vino" },
+--   }))
+-- end)
+
+
 
 return config
